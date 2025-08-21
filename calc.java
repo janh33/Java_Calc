2e6b59f00744c2cf487cbd1ebc9946a2efde0852
Janhavi Rane <janhavirane0210@gmail.com>
2025-08-21 22:20:36 +0530

    Update calc.java

--- calc.java
+++ calc.java
@@ -1,4 +1,4 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class calc{
     public static void main(String[]args){
@@ -52,4 +52,5 @@ public class calc{
                 break;
         }
     }
+
 }
